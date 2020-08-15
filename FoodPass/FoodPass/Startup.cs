b9211b08ea1b7ff6fa9eec11a23511802cd1d711
bataVeljko <marko.veljko@gmail.com>
2020-08-15 22:16:47 +0200

    Project initialization

---
+++ FoodPass/FoodPass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodPass.Startup))]
+namespace FoodPass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
